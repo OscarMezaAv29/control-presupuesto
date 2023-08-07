b497370122325d2b3a8a3d55c92ef877bf45b58b
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-08-07 10:46:40 -0600

    Creacion de ventana modal
    Creacion de ventana modal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import Presupuesto from './components/Presupuesto.vue'
 import ControlPresupuesto from './components/ControlPresupuesto.vue'
+import Modal from './components/Modal.vue'
 
+import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
+const modal = reactive({
+  mostrar: false,
+  animar: false
+})
 const presupuesto = ref(0)
 const disponible = ref(0)
 
 const definirPresupuesto = (cantidad) => {
  presupuesto.value = cantidad
  disponible.value = cantidad
+}
+
+const mostrarModal = () => {
+  modal.mostrar = true
+  modal.animar = true
+}
+
+const ocultarModal = () => {
+  modal.mostrar = false
+  modal.animar = false
 }
 </script>
 
@@ -30,6 +47,24 @@ const definirPresupuesto = (cantidad) => {
       />
     </div>
     </header>
+
+    <main v-if="presupuesto > 0">
+
+
+      <div class="crear-gasto">
+        <img
+          :src="iconoNuevoGasto"
+          alt="Icono nuevo gasto"
+          @click="mostrarModal"
+        />
+      </div>
+
+
+      <Modal
+        v-if="modal.mostrar"
+        @ocultarModal = "ocultarModal"
+      />
+    </main>
   </div>
 </template>
 
@@ -87,5 +122,15 @@ const definirPresupuesto = (cantidad) => {
     border-radius: 1.2rem;
     padding: 5rem;
   }
-  
+
+  .crear-gasto {
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+  }
+
+.crear-gasto img {
+  width: 5rem;
+  cursor: pointer;
+}
 </style>
